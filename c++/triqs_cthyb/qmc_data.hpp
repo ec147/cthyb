63b56e0715858b79975974c7a566bfc512edbe7f
Emmanuel CASTIEL <Emmanuel.CASTIEL@cea.fr>
2025-04-09 08:46:53 +0200

    Enable linear interpolation for hybridization

--- c++/triqs_cthyb/qmc_data.hpp
+++ c++/triqs_cthyb/qmc_data.hpp
@@ -59,7 +59,7 @@ namespace triqs_cthyb {
         auto ind = delta_block.mesh().to_data_index(closest_mesh_pt(dtau));
         det_scalar_t res, ya, yb;
         double xa, xb;
-        bool use_linear_interpolation = false;
+        bool use_linear_interpolation = true;
         if (use_linear_interpolation) {
           xa = delta_block.mesh().to_value(ind);
           ya = delta_block[ind](x.second, y.second);
